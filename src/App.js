dce696d104f4e989956cd601438d4f3c86ac7add
cse-amarjeet <ajroni98@gmail.com>
2023-07-14 18:08:55 +0530

    added gitignor

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import {
 } from "@web3modal/ethereum";
 import { Web3Modal, Web3Button } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { arbitrum, mainnet, polygon , sepolia} from "wagmi/chains";
-import './App.css';
-
+import { arbitrum, mainnet, polygon, sepolia } from "wagmi/chains";
+import "./App.css";
 
 const chains = [arbitrum, mainnet, polygon, sepolia];
-const projectId = "bd8a51f1348a25cc2c457fbc509bb628";
+const projectId = ""; // go to https://cloud.walletconnect.com/ and grab the project id and past it here
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiConfig = createConfig({
@@ -25,13 +24,13 @@ function App() {
 
   return (
     <div className="App">
-       <header className="App-header">
-      <WagmiConfig config={wagmiConfig}>
-        <h1>connect web3Model with Node Server</h1>
-        <Web3Button />
-      </WagmiConfig>
+      <header className="App-header">
+        <WagmiConfig config={wagmiConfig}>
+          <h1>connect web3Model with Node Server</h1>
+          <Web3Button />
+        </WagmiConfig>
 
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+        <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
       </header>
     </div>
   );
