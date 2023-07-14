46abc09f8e7d443ca9cefdbced478dc5e28539af
cse-amarjeet <ajroni98@gmail.com>
2023-07-14 18:01:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal, Web3Button } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon , sepolia} from "wagmi/chains";
+import './App.css';
+
+
+const chains = [arbitrum, mainnet, polygon, sepolia];
+const projectId = "bd8a51f1348a25cc2c457fbc509bb628";
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
+function App() {
+  // console.log(state);
+
+  return (
+    <div className="App">
+       <header className="App-header">
+      <WagmiConfig config={wagmiConfig}>
+        <h1>connect web3Model with Node Server</h1>
+        <Web3Button />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
